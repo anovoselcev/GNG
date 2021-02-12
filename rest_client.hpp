43abb68cb8799faa9a7935ca883ddb51e2be6445
smileamaxdev <novoseltsev.as@phystech.edu>
2021-02-12 21:33:32 +0300

    Initial commit

---
+++ rest_client.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <QNetworkAccessManager>
+
+namespace GNG {
+
+class RestClient{
+
+public:
+
+    RestClient() = default;
+
+    RestClient(const QString& token,
+               const QString& apiURL = RestApiURL) noexcept;
+
+    RestClient(QString&& token,
+               QString&& apiURL = "https://api-invest.tinkoff.ru/openapi") noexcept;
+
+    ~RestClient();
+
+protected:
+
+    QNetworkAccessManager*  _client;
+    QString                 _token;
+    QString                 _apiURL;
+
+    const inline static QString RestApiURL = "https://api-invest.tinkoff.ru/openapi";
+};
+
+}
